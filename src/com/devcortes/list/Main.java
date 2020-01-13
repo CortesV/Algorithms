71e8ea08d13f77de2fa0d51c57b0376038c4be8d
vsemeniuk <vitya.semenyuk1995@gmail.com>
2020-01-13 15:39:39 +0200

    fix

--- src/com/devcortes/list/Main.java
+++ src/com/devcortes/list/Main.java
@@ -1,4 +1,48 @@
 package com.devcortes.list;
 
 public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new LinkedList.Node("A");
+        list.head.next = new LinkedList.Node("B");
+        list.head.next.next = new LinkedList.Node("C");
+        list.head.next.next.next = new LinkedList.Node("D");
+        list.head.next.next.next.next = new LinkedList.Node("E");
+
+        list.printList(list.head);
+
+        LinkedList.Node prev = null;
+        LinkedList.Node current = list.head;
+
+
+        LinkedList.Node reversed = simple(current, prev);
+        list.printList(reversed);
+
+        prev = null;
+        current = reversed;
+        list.printList(recur(current, prev));
+    }
+
+    public static LinkedList.Node simple(LinkedList.Node current, LinkedList.Node prev) {
+        LinkedList.Node next = null;
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static LinkedList.Node recur(LinkedList.Node current, LinkedList.Node prev) {
+        if (current == null) {
+            return prev;
+        }
+        LinkedList.Node next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+        return recur(current, prev);
+    }
 }
